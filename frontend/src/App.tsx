4ca543dae2ff30a2485a7b52bfd4397d7c7ffc0a
vitorvnc <vitorvnc.f@gmail.com>
2022-07-13 19:21:56 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
     return(
@@ -17,4 +17,4 @@ function App() {
     )
 }
 
-export default App
+export default App;
